3097f3733e8f2238c80c5048eae98bc52085d1b0
Ahmed-msayed <ahmed.dev@icloud.com>
2023-09-15 22:32:20 +0300

    Initial Commit

---
+++ PrimoPizza/PrimoPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimoPizzaApp.swift
+//  PrimoPizza
+//
+//  Created by Ahmed Sayed on 15/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PrimoPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
